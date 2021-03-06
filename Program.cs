354524ddaa26f84e072f744b1bdebba5a82ad4a6
RD-Ricardo <ricardodias169@gmail.com>
2021-03-06 19:46:03 -0300

    Finalizando programa Gritando

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {    
+            
+            string txt;
+            Console.WriteLine("Digite gritando");
+             Console.WriteLine("--------------");
+            txt = Console.ReadLine();
+            Console.WriteLine("\n");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine(txt.ToUpper());
+            
+        
+            
+        }
+    }
+}
